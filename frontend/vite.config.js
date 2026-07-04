cb2d1a09e8454cc4a7149ab9bef97340db31f945
carlos-dani-dev <carlos.rocha@ufpi.edu.br>
2026-07-03 22:40:11 -0300

    vite configuration and github actions deploy file fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
